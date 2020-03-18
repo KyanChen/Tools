6da8e972ede4ded77a7440a7408261efeefe503c
KeyanChen1012 <keyan1055002755@163.com>
2020-03-18 13:52:12 +0800

    20200318

--- TagImage/TagImage3_0.py
+++ TagImage/TagImage3_0.py
@@ -9,10 +9,12 @@ import numpy as np
 # second, for program esc is undo a mark, mouse click and move is mark a bbox
 # third, y or Y is to save. if no object in image, just only press Y or y
 
-inPathName = r"/Users/keyanchen/Files/Dataset/标图/JPEGImages2"
-classToWrite = 1
-widthRatio = 1.
-heightRatio = 1.
+inPathName = r"F:\DataSet\WKK\sample_wkk_jx_BeiHang\8Bit"
+classes = ['bg', '2']
+classToWrite = '2'
+widthRatio = 0.5
+heightRatio = 0.4
+img_format = ['tiff']
 
 
 scalar = [(255, 0, 0), (0, 0, 255), (0, 255, 0), (200, 5, 200)]
@@ -26,7 +28,7 @@ class TagImage:
             self.inPathName = os.path.abspath(self.inPathName)
         if not os.path.exists(self.inPathName):
             os.mkdir(self.inPathName)
-        self.tempPathName = os.path.abspath(os.path.join(self.inPathName, r'.\temp'))
+        self.tempPathName = os.path.abspath(os.path.join(self.inPathName, 'temp'))
         if not os.path.exists(self.tempPathName):
             os.mkdir(self.tempPathName)
         self.curFile = self.readCurFile()
@@ -42,7 +44,7 @@ class TagImage:
         self.isDrawFinished = True
 
     def readCurFile(self):
-        txtFileName = self.tempPathName + r'\temp.txt'
+        txtFileName = os.path.join(self.tempPathName, r'temp.txt')
         if os.path.exists(txtFileName):
             with open(txtFileName, 'r') as file:
                 curFile = int(file.readline())
@@ -51,9 +53,9 @@ class TagImage:
         return curFile
 
     def writeCurFile(self, curFile):
-        txtFileName = self.tempPathName + r'\temp.txt'
+        txtFileName = os.path.join(self.tempPathName, r'temp.txt')
         with open(txtFileName, 'w') as file:
-            curFile = file.writelines(str(curFile))
+            file.writelines(str(curFile))
 
     def getFileList(self):
         fileList = []
@@ -64,8 +66,7 @@ class TagImage:
                 file_path = os.path.split(path)  # 分割出目录与文件
                 lists = file_path[1].split('.')  # 分割出文件与文件扩展名
                 file_ext = lists[-1]  # 取出后缀名(列表切片操作)
-                img_ext = ['bmp', 'jpeg', 'gif', 'psd', 'png', 'jpg', 'tif', 'tiff']
-                if file_ext in img_ext:
+                if file_ext in img_format:
                     fileList.append(path)
         fileList.sort()
         return fileList
@@ -92,22 +93,29 @@ class TagImage:
                 if not lines:
                     break
                     pass
-                data = lines.split() # 将整行数据分割处理，如果分割符是空格，括号里就不用传入参数，如果是逗号， 则传入‘，'字符。
-                # dataToRead = list(map(int, data[1:]))
-                classNum, x1, y1, x2, y2 = [int(float(x)) for x in data]
+                data = lines.strip().split()  # 将整行数据分割处理，如果分割符是空格，括号里就不用传入参数，如果是逗号， 则传入‘，'字符。
+                if not len(data):
+                    break
+                try:
+                    x1, y1, x2, y2 = list(map(int, data[1:]))
+                except ValueError:
+                    # x1, y1, x2, y2 = list(map(int, data[2:]))
+                    pass
+                class_name = data[0]
+                # class_name = '2'
+                classNum = classes.index(class_name)
                 dataToRead = [classNum, widthRatio * x1, heightRatio * y1, widthRatio * x2, heightRatio * y2]
                 dataToRead = [int(x) for x in dataToRead]
                 self.bboxList.append(dataToRead)
-                pass
 
     def saveTag(self, filePathName):
         file_path = os.path.splitext(filePathName)  # 分割出文件扩展名
         txtPathName = file_path[0] + '.txt'
         with open(txtPathName, 'w') as file:
             for i in range(0, len(self.bboxList)):
-                classNum, x1, y1, x2, y2 = self.bboxList[i]
+                class_num, x1, y1, x2, y2 = self.bboxList[i]
                 x1, y1, x2, y2 = int(x1/widthRatio), int(y1/heightRatio), int(x2/widthRatio), int(y2/heightRatio)
-                lineToWrite = str(classNum)+' '+str(x1)+' '+str(y1)+' '+str(x2)+' '+str(y2)+'\n'
+                lineToWrite = classes[class_num] + ' ' + str(x1)+' '+str(y1)+' '+str(x2)+' '+str(y2)+'\n'
                 file.writelines(lineToWrite)
 
     # 创建回调函数
@@ -125,7 +133,7 @@ class TagImage:
         elif event == cv2.EVENT_LBUTTONUP:
             self.isDrawFinished = True
             x1, y1, x2, y2 = self.getTLAndBR(x1, y1, x, y)
-            self.bboxList.append([classToWrite, x1, y1, x2, y2])
+            self.bboxList.append([classes.index(classToWrite), x1, y1, x2, y2])
 
     def getTLAndBR(self, x_1, y_1, x_2, y_2):
         if x_1 <= x_2:
@@ -139,9 +147,6 @@ class TagImage:
             elif y_1 > y_2:
                 x1, y1, x2, y2 = x_2, y_2, x_1, y_1
         return x1, y1, x2, y2
-
-
-
 
     def run(self):
         fileList = self.getFileList()
@@ -174,7 +179,6 @@ class TagImage:
                 cv2.imshow("curWindow", self.curImg)
 
                 key_pressed = cv2.waitKey(1)
-                ok_pressed = [ord('y'), ord('Y')]
                 a_pressed = [ord('a'), ord('A')]
                 d_pressed = [ord('d'), ord('D')]
                 undo_pressed = [27]
@@ -195,6 +199,7 @@ class TagImage:
                         self.bboxList.pop()
                     else:
                         continue
+
 
 def main():
     filterImg = TagImage(inPathName)
